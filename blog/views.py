f761ca4e0c71c71d8f5ebba1bfcff22931ace14b
Fatih Canbey <fcanbeyy@gmail.com>
2021-08-21 21:05:46 +0300

    models and admin panel implemented

--- blog/views.py
+++ blog/views.py
@@ -1,66 +1,25 @@
-from django.shortcuts import render
-from datetime import date
-
-all_posts = [
-    {
-        "slug": "hiking-in-the-mountains",
-        "image": "mountains.jpg",
-        "author": "Dyrean",
-        "date": date(2021, 8, 20),
-        "title": "Mountain Hiking",
-        "excerpt": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-        "content": '''
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est architecto
-            quae quidem natus odio accusantium placeat eum eaque, quo tenetur
-            consectetur sint unde non aspernatur, facere fuga labore quis sequi!
-            '''
-    },
-    {
-        "slug": "programming-is-fun",
-        "image": "coding.jpg",
-        "author": "Dyrean",
-        "date": date(2021, 8, 18),
-        "title": "Programming Is Great",
-        "excerpt": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-        "content": '''
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est architecto
-            quae quidem natus odio accusantium placeat eum eaque, quo tenetur
-            consectetur sint unde non aspernatur, facere fuga labore quis sequi!
-            '''
-    },
-    {
-        "slug": "into-the-woods",
-        "image": "woods.jpg",
-        "author": "Dyrean",
-        "date": date(2021, 8, 22),
-        "title": "Nature At Its Best",
-        "excerpt": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-        "content": '''
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est architecto
-            quae quidem natus odio accusantium placeat eum eaque, quo tenetur
-            consectetur sint unde non aspernatur, facere fuga labore quis sequi!
-            '''
-    }
-]
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 # Create your views here.
 
-def get_date(post):
-    return post['date']
 
 def starting_page(request):
-    sorted_posts = sorted(all_posts, key = lambda x: x['date'])
-    latest_posts = sorted_posts[-3:]
+    latest_posts = Post.objects.all().order_by("-date")[:3]
     return render(request, "blog/index.html", {
         "posts": latest_posts
     })
 
+
 def posts(request):
+    all_posts = Post.objects.all().order_by("-date")
     return render(request, "blog/all-posts.html", {
         "all_posts": all_posts
     })
 
+
 def post_detail(request, slug):
-    identified_post = next(post for post in all_posts if post['slug'] == slug)
-    return render(request, "blog/post-detail.html",{
-        "post": identified_post
+    identified_post = get_object_or_404(Post, slug=slug)
+    return render(request, "blog/post-detail.html", {
+        "post": identified_post,
+        "post_tags": identified_post.tags.all()
     })
